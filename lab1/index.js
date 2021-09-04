e29274394ce14444baab2765ef5099da7cec6f54
LilDev1l <lildev1l.ya@gmail.com>
2021-09-05 00:00:38 +0300

    lab1 done

---
+++ lab1/index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const PORT = 3000;
+
+const server = http.createServer( (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    res.write('<h1>Hello World</h1>');
+    res.write('<br>Method: ' + req.method);
+    res.write('<br>URL: ' + req.url);
+
+    res.end();
+
+});
+
+server.listen(PORT, () => {
+    console.log(`Server has been started on ${PORT}...`)
+})
